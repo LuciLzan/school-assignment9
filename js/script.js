c0245ba94c9f7e6f436d82fa6b5ad6f799c71bfb
Luke Laurenzana <laurenzana.luke@ic.edu>
2025-10-05 21:33:26 -0500

    fixed gifs not loading due to CORB

--- js/script.js
+++ js/script.js
@@ -5,19 +5,25 @@ registerButtons()
 function registerButtons() {
     let button = document.querySelector("#fetch-gif-btn")
     button.addEventListener("click", () => {
-        //Professor said to use hard coded API key just this once
+        let gifOutput = document.querySelector("#gif-container")
         let search = document.querySelector("#search-input").value
-        let endPoint = `https://api.giphy.com/v1/gifs/search?api_key=${SUPER_SECRET_API_KEY_HARD_CODED_IN_PLAINTEXT_DO_NOT_UNDER_ANY_CIRCUMSTANCES_INCLUDE_IN_PRODUCTION_BECAUSE_OF_OBVIOUS_SECURITY_RISKS}&q=${search}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
-        fetch(endPoint).then(async data => {
-            let gifHTML = ""
-            let gifs = await data.json()
-            let gifOutput = document.querySelector("#gif-container")
-            for(let gif of gifs.data) {
-                console.log(gif)
-                gifOutput.innerHTML += `<img src=${gif.embed_url} alt="${gif.title}" class = "col-3 mb-3">`
-            }
+        if(search !== "") {
+            gifOutput.innerHTML = "Loading..."
+            //Professor said to use hard coded API key just this once
+            let endPoint = `https://api.giphy.com/v1/gifs/search?api_key=${SUPER_SECRET_API_KEY_HARD_CODED_IN_PLAINTEXT_DO_NOT_UNDER_ANY_CIRCUMSTANCES_INCLUDE_IN_PRODUCTION_BECAUSE_OF_OBVIOUS_SECURITY_RISKS}&q=${search}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+            fetch(endPoint).then(async data => {
+                let gifHTML = ""
+                let gifs = await data.json()
 
-        })
+                gifOutput.innerHTML= ""
+                for(let gif of gifs.data) {
+                    gifOutput.innerHTML += `<iframe src=${gif.embed_url} class = "col-3 mb-3">`
+                }
+
+            })
+        }else {
+            window.alert("Please enter a search parameter")
+        }
 
     })
 }
