429f923267f3b9886cebba1b00fd0c6fd57beba0
Luke Laurenzana <laurenzana.luke@ic.edu>
2025-10-05 21:42:48 -0500

    removed unused variable

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ function registerButtons() {
             //Professor said to use hard coded API key just this once
             let endPoint = `https://api.giphy.com/v1/gifs/search?api_key=${SUPER_SECRET_API_KEY_HARD_CODED_IN_PLAINTEXT_DO_NOT_UNDER_ANY_CIRCUMSTANCES_INCLUDE_IN_PRODUCTION_BECAUSE_OF_OBVIOUS_SECURITY_RISKS}&q=${search}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
             fetch(endPoint).then(async data => {
-                let gifHTML = ""
                 let gifs = await data.json()
 
                 gifOutput.innerHTML= ""
